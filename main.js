371d0938d2d3630d9af727ddfb2f59b36bce448e
awang94 <adam.wang94@gmail.com>
2014-10-19 02:43:08 -0400

    Update main.js
    Added mockup button functionality.

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 $( document ).ready( function(){
      $("#description").hide();
+     $("#mockup").hide();
 });
 function showGame(){
     $("#members").fadeToggle(200);
     $("#start").fadeToggle(200);
     setTimeout(function(){
         $("#description").fadeIn('slow');
+    },300);
+       return false;
+    };
+function showMockup(){
+    $("#description").fadeToggle(200);
+    setTimeout(function(){
+        $("#mockup").fadeIn('slow');
     },300);
        return false;
     };
